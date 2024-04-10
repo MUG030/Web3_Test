ac20d86e4a7d81946f97611f71df997c96457fb4
MUG <ys8267928563@outlook.jp>
2024-04-10 22:49:11 +0900

    セットアップ + 課題01
    12x12の掛け算表を出力する関数を作成します。 できれば、この関数をクラスメソッドとして実装しましょう。

---
+++ Test_01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Test01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MulTable mulTable = new MulTable();
+            mulTable.PrintTable();
+        }
+    }
+
+    public class MulTable
+    {
+        public void PrintTable()
+        {
+            for (int i = 1; i <= 12; i++)
+            {
+                for (int j = 1; j <= 12; j++)
+                {
+                    Console.Write($"{i * j}\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
